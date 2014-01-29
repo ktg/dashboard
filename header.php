a0572a2c02168de8c893db4e6af71198816a27ce
Kevin Glover <ktg@cs.nott.ac.uk>
2014-01-29 14:18:43 +0000

    Small header fix

--- header.php
+++ header.php
@@ -60,8 +60,8 @@ add_filter( 'show_admin_bar', '__return_false' );
 				<li><a href="<?php echo get_page_link(get_page_by_title(discover)->ID); ?>">Getting Started</a></li>
 				<li class="active"><a href="">Dashboard</a></li>		
 				<?php elseif($post->post_parent): ?>
-				<li><a href="<?php echo get_page_link(get_page_by_title(discover)->ID); ?>">Getting Started</a></li>
-				<li class="active"><a href="<?php echo get_page_link(get_page_by_title(dashboard)->ID); ?>">Dashboard</a></li>
+				<li class="active"><a href="<?php echo get_page_link(get_page_by_title(discover)->ID); ?>">Getting Started</a></li>
+				<li><a href="<?php echo get_page_link(get_page_by_title(dashboard)->ID); ?>">Dashboard</a></li>
 				<?php else: ?>				
 				<li class="active"><a href="">Getting Started</a></li>
 				<li><a href="<?php echo get_page_link(get_page_by_title(dashboard)->ID); ?>">Dashboard</a></li>
