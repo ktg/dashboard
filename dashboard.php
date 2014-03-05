165e9318f073c616e3aee86436f0b400b849bf00
Kevin Glover <ktg@cs.nott.ac.uk>
2014-03-05 15:16:10 +0000

    Facebook Page login

--- dashboard.php
+++ dashboard.php
@@ -38,8 +38,10 @@ $dashboard_view_path = site_url ('wp-content/themes/' . $theme . '/images/dashbo
 							$service_path = $services_path . $service->key . "/";
 							$icon = $service_path . "images/icon.png";
 							$service_include = "services/" . $service->key . "/include.php";
-							echo $service_include;
-							include $service_include;
+                            if(file_exists($service_include))
+                            {
+							    include $service_include;
+							}
 							$service_page = $service_path . "page.php";
 							?>
 
