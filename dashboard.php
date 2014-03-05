e8dc814da337ef174f2c417975f6db09647e998d
Kevin Glover <ktg@cs.nott.ac.uk>
2014-03-05 15:29:29 +0000

    Facebook Page login

--- dashboard.php
+++ dashboard.php
@@ -96,9 +96,9 @@ function load_page(service_key)
                     {
                         $service_path = $services_path . $service->key . "/";
                         $service_page = $service_path . "page.php";
-                        if($_GET["code"]
+                        if($_GET["code"])
                         {
-                            $service_page = $service_page . "?code=" .$_GET['code'] . "&state=" . $_GET['state'];
+                            $service_page = "$service_page?code=$_GET[code]&state=$_GET[state]";
                         }
                         ?>
                         if(service_key == '<?php echo $service->key ?>')
