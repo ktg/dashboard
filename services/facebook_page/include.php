1ce15f81401d348767955adcea94cb74382ffed5
Kevin Glover <ktg@cs.nott.ac.uk>
2014-03-05 15:37:47 +0000

    Facebook Page login

---
+++ services/facebook_page/include.php
@@ -0,0 +1,13 @@
+<?php
+require 'facebook/facebook.php';
+
+$facebook = new Facebook(array(
+        'appId'  => '342794992525201',
+        'secret' => 'e69e8a79972fe7330a40d8ab1d68994f'
+));
+
+$user = $facebook->getUser();
+
+echo $user;
+echo $facebook->getAccessToken();
+?>
